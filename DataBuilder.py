eba2697458dc39a66a78c7fb43996fbf2de2f772
Guilherme Costa <guilherme.costa@sap.com>
2023-08-02 11:13:37 +0100

    add black

--- DataBuilder.py
+++ DataBuilder.py
@@ -2,16 +2,18 @@ import csv
 import torch
 import tqdm
 
-class DataBuilder:
-    def __init__(self, triples_path,entities_path,relations_path, NLP_client, graph_path=None):
-        def _read_triples_file(self, triples_path,entities_path,relations_path):
 
+class DataBuilder:
+    def __init__(
+        self, triples_path, entities_path, relations_path, NLP_client, graph_path=None
+    ):
+        def _read_triples_file(self, triples_path, entities_path, relations_path):
             entity = []
             entity_label = []
             with open(entities_path, "r", encoding="utf8") as fin:
                 reader = csv.reader(fin)
-                
-                #skip the header
+
+                # skip the header
                 header = next(reader)
 
                 for row in reader:
@@ -24,7 +26,7 @@ class DataBuilder:
             with open(relations_path, "r", encoding="utf8") as fin:
                 reader = csv.reader(fin)
 
-                #skip the header
+                # skip the header
                 header = next(reader)
 
                 for row in reader:
@@ -36,20 +38,18 @@ class DataBuilder:
             relation = []
             object = []
 
-            nrow = sum(1 for _ in open(triples_path, 'r', encoding='utf-8'))
+            nrow = sum(1 for _ in open(triples_path, "r", encoding="utf-8"))
             with open(triples_path, "r", encoding="utf8") as fin:
-                
-
                 for line in tqdm(fin, total=nrow):
-                    ls = line.strip().split(',')
-                    
+                    ls = line.strip().split(",")
+
                     subject.append(entity2id[ls[0]])
                     relation.append(relation2id[ls[1]])
                     object.append(entity2id[ls[2]])
-            edge_index = torch.stack((torch.tensor(subject),torch.tensor(object)))
+            edge_index = torch.stack((torch.tensor(subject), torch.tensor(object)))
             edge_type = torch.tensor(relation)
 
-            return edge_index,edge_type
+            return edge_index, edge_type
 
         def _get_feature_label(self, label):
             return self.NLP_client(label)
--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
-vad_csv_path = './data/VAD_kg.csv'
-vad_entity_path = './data/vad_entities_labels.csv'
-vad_relation_path = './data/vad_properties_labels.csv'
+vad_csv_path = "./data/VAD_kg.csv"
+vad_entity_path = "./data/vad_entities_labels.csv"
+vad_relation_path = "./data/vad_properties_labels.csv"
